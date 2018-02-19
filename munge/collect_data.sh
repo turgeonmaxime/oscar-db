22959d958a0d6b8f584d7e1798de376d2ae5f9d1
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2018-02-19 09:59:42 -0600

    add bash script to run scrapers

---
+++ munge/collect_data.sh
@@ -0,0 +1,13 @@
+# Industry awards
+Rscript web-scrapers/BAFTA_scraper.R
+Rscript web-scrapers/DGA_scraper.R
+Rscript web-scrapers/golden_globes_scraper.R
+Rscript web-scrapers/PGA_scraper.R
+Rscript web-scrapers/SGA_scraper.R
+
+# Critics awards
+Rscript web-scrapers/NYFCC_scraper.R
+Rscript web-scrapers/LAFCA_scraper.R
+Rscript web-scrapers/NSFC_scraper.R
+Rscript web-scrapers/CFCA_scraper.R
+Rscript web-scrapers/NBR_scraper.R
